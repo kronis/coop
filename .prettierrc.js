ce8ac61905025afc899ce52552a82b820819ff73
Markus Loupeen <kronis85@gmail.com>
2024-10-11 22:18:28 +0200

    Add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: 'always',
+};
